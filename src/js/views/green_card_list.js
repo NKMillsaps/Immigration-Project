901f97703593054cbea13eaa6bf6ef572d66e1d4
NKMillsaps <nkmillsaps@gmail.com>
2019-06-15 20:54:51 +0000

    green card list 2

--- src/js/views/green_card_list.js
+++ src/js/views/green_card_list.js
@@ -13,29 +13,46 @@ export class Green_card_list extends React.Component {
 							<h2 className="card-title">
 								<u>Application to Register Permanent Residence or Adjust Status (I-485)</u>
 							</h2>
-							<p className="card-text">
-								The Full List of Document required by USCIS. I-130 (Petition for Alien Relative), I-130A
-								(Supplemental Information for Spouse Beneficiary), I-864 (Affidavit of Support), I-485
-								(Application to Register Permanent Residence or Adjust Status), I-693 (Report of Medical
-								Examination and Vaccination Record (SEALED), I-765 (Application for Employment
-								Authorization). Contents include: 1. Form G1145 E-Notification of Application/Petition
-								Acceptance 2. Check in the amount of US $ 535.00 for form I-130 3. Check in the amount
-								of US $ 1225.00 for form I-485 4. I-130 Petition for Alien Relative • Copy of
-								Petitioners( US Citizen) passport • Copy of Marriage Certificate • Copy of Termination
-								of Previous Marriage • Two photos • Affidavits sworn or affirmed by third parties •
-								G-1145 e-Notification of Application 5. I-130A Supplemental Information for Spouse
-								Beneficiary • Copy of Beneficiary&#39;s (Alien Spouse) passport • Two photos • Copy of
-								I-94 • Copy of I-797C (Notice of Action) • G-1145 e-Notification of Application 6. I-864
-								Affidavit of Support • Copy of the passport • Copy of W2 2016 • Copy of U.S. Individual
-								Income Tax Return 2016 7. I-485 Application to Register Permanent Residence Or Adjust
-								Status • Copy of the Passport • Copy of Marriage Certificate • Two photos • Copy of I-94
-								• Copy of I-797C (Notice of Action) • Copy of the Visa • Copy of Birth Certificate with
-								certified translation • Affidavits sworn or affirmed by third parties • Joint Bank
-								Account Statement • G-1145 e-Notification of Application 8. I-693 Report of Medical
-								Examination and Vaccination Record (Sealed) 9. I-765 Application For Employment
-								Authorization • Copy of the Passport • Copy of the Visa • Copy of I-94 • Two photos •
-								Copy of Birth Certificate with certified translation
-							</p>
+
+							<ol>
+								The Full List of Document required by USCIS.
+								<li> I-130 (Petition for Alien Relative),</li>
+								<li>
+									{" "}
+									I-130A (Supplemental Information for Spouse Beneficiary), I-864 (Affidavit of
+									Support), I-485 (Application to Register Permanent Residence or Adjust Status),{" "}
+								</li>
+								<li>I-693 (Report of Medical Examination and Vaccination Record (SEALED)</li>
+								<li> I-765 (Application for Employment Authorization).</li>
+								Contents include:
+								<li> Form G1145 E-Notification of Application/Petition Acceptance </li>
+								<li> Check in the amount of US $ 535.00 for form I-130 </li>{" "}
+								<li> Check in the amount of US $ 1225.00 for form I-485 </li>{" "}
+								<li> I-130 Petition for Alien Relative</li>
+								<li> Copy of Petitioners( US Citizen) passport</li>
+								<li> Copy of Marriage Certificate </li>
+								<li>Copy of Termination of Previous Marriage </li> <li> Two photos</li>{" "}
+								<li>Affidavits sworn or affirmed by third parties </li>{" "}
+								<li> G-1145 e-Notification of Application </li>
+								<li> I-130A Supplemental Information for Spouse Beneficiary </li>•{" "}
+								<li>Copy of Beneficiary&#39;s (Alien Spouse) passport</li> <li>Two photos</li>{" "}
+								<li> Copy of I-94</li> <li> Copy of I-797C (Notice of Action)</li>{" "}
+								<li>G-1145 e-Notification of Application</li> <li> I-864 Affidavit of Support</li>{" "}
+								<li>Copy of the passport</li> <li> Copy of W2 2016</li>{" "}
+								<li> Copy of U.S. Individual Income Tax Return 2016 </li>
+								<li> I-485 Application to Register Permanent Residence Or Adjust Status</li> •{" "}
+								<li>Copy of the Passport</li> •<li>Copy of Marriage Certificate</li> •{" "}
+								<li>Two photos</li> • <li>Copy of I-94</li> • <li>Copy of I-797C (Notice of Action)</li>{" "}
+								• <li>Copy of the Visa</li>{" "}
+								<li> Copy of Birth Certificate with certified translation </li>
+								<li>Affidavits sworn or affirmed by third parties </li>
+								<li> Joint Bank Account Statement</li>{" "}
+								<li> G-1145 e-Notification of Application (optional)</li>{" "}
+								<li> I-693 Report of Medical Examination and Vaccination Record (Sealed)</li>{" "}
+								<li> I-765 Application For Employment Authorization</li> <li> Copy of the Passport </li>
+								<li>Copy of the Visa</li> <li>Copy of I-94</li> <li>Two photos</li>{" "}
+								<li>Copy of Birth Certificate with certified translation</li>
+							</ol>
 
 							<p className="text-center">
 								<Link to="/i130apetition">
