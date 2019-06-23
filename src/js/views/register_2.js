b54c0cc34a433b84e072600ce248e3109ab205b0
NKMillsaps <nkmillsaps@gmail.com>
2019-06-23 18:57:51 +0000

    registration 2 changed

--- src/js/views/register_2.js
+++ src/js/views/register_2.js
@@ -15,9 +15,9 @@ export class Register_2 extends React.Component {
 							return store.demo.map((item, index) => {
 								return (
 									<form key={index}>
-										<div className="form-group">
+										<div className="form-group mx-auto p-2">
 											<div className="form-row">
-												<div className="col-md-7 mb-3">
+												<div className="col-md mb-3">
 													<label htmlFor="inputEmail">Email Address</label>
 													<input
 														type="email"
@@ -28,7 +28,8 @@ export class Register_2 extends React.Component {
 														required
 													/>
 												</div>
-
+											</div>
+											<div className="form-row">
 												<div className="col-md mb-3">
 													<label htmlFor="inputPassword3">Password</label>
 
@@ -41,33 +42,7 @@ export class Register_2 extends React.Component {
 													/>
 												</div>
 											</div>
-											<div className="form-row">
-												<div className="col-md-7 mb-3">
-													<label htmlFor="inputEmail">Email Address (again)</label>
-													<input
-														type="email"
-														name="email"
-														className="form-control"
-														id="inputEmail"
-														placeholder="email"
-														required
-													/>
-												</div>
 
-												<div className="col-md mb-3">
-													<label htmlFor="inputPassword3">Password (again)</label>
-
-													<input
-														type="password"
-														name="password"
-														className="form-control"
-														id="inputPassword3"
-														placeholder="Password"
-													/>
-												</div>
-											</div>
-
-											<hr />
 											<div className="form-row">
 												<div className="col-md mb-3">
 													<label htmlFor="validationDefault01">Last Name (Family name)</label>
@@ -110,219 +85,6 @@ export class Register_2 extends React.Component {
 											</div>
 
 											<div className="form-row">
-												<div className="col-md-6 mb-3">
-													<label htmlFor="inputAddress">U.S. Mailing Address</label>
-													<input
-														type="text"
-														name="US_Address"
-														className="form-control"
-														id="inputAddress"
-														placeholder="Number, Street name"
-													/>
-												</div>
-
-												<div className="form-inline col-md">
-													<div className="form-check form-check-inline">
-														<input
-															className="form-check-input"
-															type="checkbox"
-															name="apartment"
-															id="inlinecheckbox1"
-															value="option1"
-														/>
-														<label className="form-check-label" htmlFor="inlinecheckbox1">
-															Apartment
-														</label>
-													</div>
-													<div className="form-check form-check-inline">
-														<input
-															className="form-check-input"
-															type="checkbox"
-															name="apartment"
-															id="inlinecheckbox2"
-															value="option2"
-														/>
-														<label
-															className="form-check-label col-md"
-															htmlFor="inlinecheckbox2">
-															Suite
-														</label>
-													</div>
-													<div className="form-check form-check-inline">
-														<input
-															className="form-check-input"
-															type="checkbox"
-															name="apartment"
-															id="inlinecheckbox3"
-															value="option3"
-														/>
-														<label
-															className="form-check-label col-md"
-															htmlFor="inlinecheckbox3">
-															Floor
-														</label>
-													</div>
-													<input
-														type="text"
-														className="form-control col-sm"
-														placeholder="#"
-													/>
-												</div>
-											</div>
-											<div className="form-row">
-												<div className="col-md-6 mb-3">
-													<label htmlFor="validationDefault03">City/Town</label>
-													<input
-														type="text"
-														name="city"
-														className="form-control"
-														id="validationDefault03"
-														required
-													/>
-												</div>
-
-												<div className="col-md-3 mb-3">
-													<label htmlFor="inputState">State</label>
-													<select
-														id="inputState"
-														className="form-control required"
-														name="state">
-														<option defaultValue="">Choose...</option>
-														<option value="AL">AL</option>
-														<option value="AK">AK</option>
-														<option value="AZ">AZ</option>
-														<option value="AR">AR</option>
-														<option value="CA">CA</option>
-														<option value="CO">CO</option>
-														<option value="CT">CT</option>
-														<option value="DE">DE</option>
-														<option value="DC">DC</option>
-														<option value="FL">FL</option>
-														<option value="GA">GA</option>
-														<option value="HI">HI</option>
-														<option value="ID">ID</option>
-														<option value="IL">IL</option>
-														<option value="IN">IN</option>
-														<option value="IA">IA</option>
-														<option value="KS">KS</option>
-														<option value="KY">KY</option>
-														<option value="LA">LA</option>
-														<option value="ME">ME</option>
-														<option value="MD">MD</option>
-														<option value="MA">MA</option>
-														<option value="MI">MI</option>
-														<option value="MN">MN</option>
-														<option value="MS">MS</option>
-														<option value="MO">MO</option>
-														<option value="MT">MT</option>
-														<option value="NE">NE</option>
-														<option value="NV">NV</option>
-														<option value="NH">NH</option>
-														<option value="NJ">NJ</option>
-														<option value="NM">NM</option>
-														<option value="NY">NY</option>
-														<option value="NC">NC</option>
-														<option value="ND">ND</option>
-														<option value="OH">OH</option>
-														<option value="OK">OK</option>
-														<option value="OR">OR</option>
-														<option value="PA">PA</option>
-														<option value="RI">RI</option>
-														<option value="SC">SC</option>
-														<option value="SD">SD</option>
-														<option value="TN">TN</option>
-														<option value="TX">TX</option>
-														<option value="UT">UT</option>
-														<option value="VT">VT</option>
-														<option value="VA">VA</option>
-														<option value="WA">WA</option>
-														<option value="WV">WV</option>
-														<option value="WI">WI</option>
-														<option value="WY">WY</option>
-														<option>...</option> required
-													</select>
-												</div>
-												<div className="col-md-3 mb-3">
-													<label htmlFor="validationDefault05">Zip code</label>
-													<input
-														type="number"
-														name="zip"
-														className="form-control"
-														id="validationDefault05"
-														placeholder="Zip"
-														maxLength="5"
-														required
-													/>
-												</div>
-											</div>
-											<div className="form-row">
-												<div className="form-inline col-sm">
-													<div className="form-check form-check-inline">
-														<h1>Sex &nbsp;</h1>
-														<input
-															className="form-check-input"
-															type="checkbox"
-															name="sex"
-															id="inlinecheckbox1"
-															value="option1"
-														/>
-														<label className="form-check-label" htmlFor="inlinecheckbox1">
-															Male
-														</label>
-													</div>
-
-													<div className="form-check form-check-inline">
-														<input
-															className="form-check-input"
-															type="checkbox"
-															name="sex"
-															id="inlinecheckbox3"
-															value="option3"
-														/>
-														<label
-															className="form-check-label col-md"
-															htmlFor="inlinecheckbox3">
-															Femail
-														</label>
-													</div>
-												</div>
-												<div className="col-md mb-3">
-													<label htmlFor="validationDefault03">Date of Birth</label>
-													<input
-														type="date"
-														name="dateOfBirth"
-														className="form-control"
-														id="validationDefault03"
-														placeholder="MM/DD/YYYY"
-														required
-													/>
-												</div>
-												<div className="col-md mb-3">
-													<label htmlFor="validationDefault03">Country of Birth</label>
-													<input
-														type="text"
-														name="countryOfBirth"
-														className="form-control"
-														id="validationDefault03"
-														placeholder="Country of birth"
-														required
-													/>
-												</div>
-												<div className="col-md mb-3">
-													<label htmlFor="validationDefault03">
-														Country of Nationality/Citizenship
-													</label>
-													<input
-														type="text"
-														name="citizenship"
-														className="form-control"
-														id="validationDefault03"
-														placeholder="Nationality/Citizenship"
-														required
-													/>
-												</div>
-											</div>
-											<div className="form-row">
 												<div className="col-md mb-3">
 													<label htmlFor="validationDefault01">Daytime Phone Number</label>
 													<input
@@ -334,7 +96,8 @@ export class Register_2 extends React.Component {
 														maxLength="12"
 													/>
 												</div>
-
+											</div>
+											<div className="form-row">
 												<div className="col-md mb-3">
 													<label htmlFor="validationDefault02">Mobile Phone Number</label>
 													<input
@@ -347,18 +110,18 @@ export class Register_2 extends React.Component {
 													/>
 												</div>
 											</div>
-										</div>
-										<div className="form-group row">
-											<div className="col-sm-10">
-												<button
-													type="submit"
-													className="btn btn-primary"
-													onClick={() => actions.addPerson()}>
-													Create user
-												</button>
+											<hr />
+											<div className="form-row">
+												<div className="col-sm-10">
+													<button
+														type="submit"
+														className="btn btn-primary float-right"
+														onClick={() => actions.addPerson()}>
+														Create user
+													</button>
+												</div>
 											</div>
 										</div>
-										<div />
 									</form>
 								);
 							});
