f4f1307dc51da3677c050c36fdc64729b814023c
NKMillsaps <nkmillsaps@gmail.com>
2019-07-09 20:41:56 +0000

    navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -7,75 +7,91 @@ import { Context } from "../store/appContext";
 export class Navbar extends React.Component {
 	render() {
 		return (
-			<nav className="navbar sticky-top navbar-expand-lg mb-1 navbar-dark bg-dark">
-				<Link to="/">
-					<img src={logo} />
-				</Link>
+			<nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<Link to="/">
+						<img src={logo} />
+					</Link>
 
-				{/*<Link to="/modalf1">
+					{/*<Link to="/modalf1">
 						<select className="custom-select">
 							<option selected>Languages</option>
 							<option value={1}>Spanish</option>
 							<option value={2}>Russian</option>
 						</select>
 					</Link>*/}
-				<Context.Consumer>
-					{({ store, actions }) => {
-						return (
-							<div>
-								<ul className="nav bd-sidenav text-center">
-									<li className="nav-item">
-										<a className="nav-link active text-primary" href="##how-it-works">
-											Contacts
-										</a>
-									</li>
-									<li className="nav-item">
-										<Link className="nav-link text-primary" to="/doctors">
-											Doctors
-										</Link>
-									</li>
-									<li className="nav-item">
-										<a className="nav-link text-primary" href="#">
-											Translation Agencies
-										</a>
-									</li>
-									<li className="nav-item">
-										<a className="nav-link text-primary" href="#">
-											Spanish{" "}
-										</a>
-									</li>
-									<li className="nav-item">
-										<a className="nav-link text-primary" href="#">
-											Russian{" "}
-										</a>
-									</li>
+					<Context.Consumer>
+						{({ store, actions }) => {
+							return (
+								<div className="tops">
+									<ul className="navbar-nav mr-auto">
+										<li className="nav-item">
+											<a className="nav-link active text-primary" href="##how-it-works">
+												Contacts
+											</a>
+										</li>
+										<li className="nav-item">
+											<Link className="nav-link text-primary" to="/doctors">
+												Doctors
+											</Link>
+										</li>
+										<li className="nav-item">
+											<a className="nav-link text-primary" href="#">
+												Translation Agencies
+											</a>
+										</li>
+										<li className="nav-item">
+											<a className="nav-link text-primary" href="https://www.uscis.gov">
+												USCIS{" "}
+											</a>
+										</li>
+										<li className="nav-item dropdown">
+											<a
+												className="nav-link dropdown-toggle"
+												href="#"
+												id="navbarDropdown"
+												role="button"
+												data-toggle="dropdown"
+												aria-haspopup="true"
+												aria-expanded="false">
+												Language
+											</a>
+											<div className="dropdown-menu" aria-labelledby="navbarDropdown">
+												<a className="dropdown-item" href="#">
+													Spanish
+												</a>
+												<a className="dropdown-item" href="#">
+													Russian
+												</a>
+											</div>
+										</li>
 
-									<li className="nav-item">
-										<a className="nav-link text-primary" href="https://www.uscis.gov">
-											USCIS{" "}
-										</a>
-									</li>
-									<li className="nav-item">
-										<a className="nav-link text-primary" href="/docs/4.0/about/overview/">
-											About
-										</a>
-									</li>
-								</ul>
-								{!store.token ? (
-									<Link to="/loginRegister">
-										<button className="btn btn-primary m-3">Login/Register</button>
-									</Link>
-								) : (
-									<Link to="/loginRegister">
-										<button className="btn btn-primary m-3" onClick={() => actions.logoutUser()}>
-											Logout
-										</button>
-									</Link>
-								)}
-							</div>
-						);
-					}}
-				</Context.Consumer>
+										<li className="nav-item">
+											<a className="nav-link text-primary" href="/docs/4.0/about/overview/">
+												About
+											</a>
+										</li>
+										<form className="form-inline my-2 my-lg-0">
+											{!store.token ? (
+												<Link to="/loginRegister">
+													<button className="btn btn-primary m-3">Login/Register</button>
+												</Link>
+											) : (
+												<Link to="/loginRegister">
+													<button
+														className="btn btn-primary m-3"
+														onClick={() => actions.logoutUser()}>
+														Logout
+													</button>
+												</Link>
+											)}
+										</form>
+									</ul>
+								</div>
+							);
+						}}
+					</Context.Consumer>
+				</div>
 			</nav>
 		);
 	}
